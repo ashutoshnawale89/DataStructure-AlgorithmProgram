98e2acd807b824e08820e730b4a7852c62fc5911
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-08-02 23:02:59 +0530

    Prime Number 1-1000

---
+++ PrimeNumber.java
@@ -0,0 +1,31 @@
+package com.Algoritham.program;
+
+public class PrimeNumber {
+	static int m=1;
+	public static void primenum(int upperlimit) {
+	int i=0;
+	int num=0;
+	for (i=1; i<= upperlimit; i++) {
+		int count=0;
+		for (num=i;num>=1; num--) {
+			if (i%num==0) {
+				count=count+1;
+			}
+		}
+		if(count==2) {
+			System.out.println(i++);		
+		}
+	}
+	}
+	public static void main (String[]args)   {
+		PrimeNumber obj=new PrimeNumber();
+	     obj.primenum(1000);
+	   
+	 }
+}
+
+
+
+
+
+
